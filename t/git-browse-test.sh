3ab6487ab4d8af125620526d6ccfc6b3f6e105dc
Łukasz Korecki <lukasz@coffeesounds.com>
2013-10-20 22:13:56 +0100

    fix compare url

--- t/git-browse-test.sh
+++ t/git-browse-test.sh
@@ -17,44 +17,43 @@ commit="1e90d9e"
 
 testBasic() {
   local url=$(NOOPEN=y $gitBrowsePath)
-  PP "Test basic repository url"
+  PP "Basic functionality check"
   Check "assertEquals" "$url" "$prefix/tree/$currentBranch/"
 }
 
 testUrlencode() {
   local url=$(NOOPEN=y $gitBrowsePath -p)
-  PP "Test urlencoding branch name in pull request url"
+  PP "Branches need to be url encoded when opening pull request urls"
   Check "assertEquals" "$url" "$prefix/pull/$currentUrlencodedBranch"
 }
 
 testCommitUrl() {
   local url=$(NOOPEN=y $gitBrowsePath -c $commit)
-  PP "Test commit url"
   Check "assertEquals" "$url" "$prefix/commit/$commit"
 }
 
 testFileUrl() {
   local url=$(NOOPEN=y $gitBrowsePath $workingDir/../git-browse)
-  PP "Test file url"
+  PP "Link to a file"
   Check "assertEquals" "$url" "$prefix/tree/$currentBranch/git-browse"
 }
 
 testFileUrlWithRange() {
   local url=$(NOOPEN=y $gitBrowsePath -r20,30 $workingDir/../git-browse)
-  PP "Test file url with range"
+  PP "Link to a file with line range"
   Check "assertEquals" "$url" "$prefix/tree/$currentBranch/git-browse#L20-L30"
 }
 
 testFileUrlWithLine() {
   local url=$(NOOPEN=y $gitBrowsePath -r20 $workingDir/../git-browse)
-  PP "Test file url with range"
+  PP "Link to a specific line in a file"
   Check "assertEquals" "$url" "$prefix/tree/$currentBranch/git-browse#L20"
 }
 
 testCompareURL() {
   local url=$(NOOPEN=y $gitBrowsePath -C test)
-  PP "Test compare url"
-  Check 'assertEquals' "$url" "$prefix/compare/test...$currentBranch"
+  PP "Open branch comparison url"
+  Check 'assertEquals' "$url" "$prefix/compare/$(echo test | urlEncode)...$currentUrlencodedBranch"
 }
 
 Run
